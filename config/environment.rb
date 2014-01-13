f7b2190b7aae3304a76198a21ec756189f7452ee
Yosef Maayan <10tklz@gmail.com>
2014-01-13 17:01:11 -0500

    Changed route

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 DemoApp::Application.initialize!
+
+
+# force Rails into production mode when                          
+# you don't control web/app server and can't set it the proper way                  
+ENV['RAILS_ENV'] ||= 'production'
